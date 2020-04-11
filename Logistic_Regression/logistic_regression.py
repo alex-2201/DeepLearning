41e98e99d1dc02c2ab758f0d9a62ec23766a2e6f
alex-2201 <n.vanson.2201@gmail.com>
2020-04-11 13:54:36 +0700

    softmax

--- Logistic_Regression/logistic_regression.py
+++ Logistic_Regression/logistic_regression.py
@@ -3,3 +3,29 @@ import numpy as np
 
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))
+
+
+def sigmoid_derivative(x):
+    s = sigmoid(x)
+    ds = s * (1 - s)
+    return ds
+
+
+def image2vector(image):
+    v = image.reshape(image.shape[0] * image.shape[1] * image.shape[2], 1)
+    return v
+
+
+def normalizeRows(x):
+    x_norm = np.linalg.norm(x, ord=2, axis=1, keepdims=True)
+    x = x / x_norm
+    return x
+
+
+def sofmax(x):
+    x_exp = np.exp(x)
+    x_sum = np.sum(x_exp, axis=1, keepdims=True)
+    s = x_exp / x_sum
+    return s
+
+
