4fe0d3843bac67f582921b888f2119da97176568
alex-2201 <n.vanson.2201@gmail.com>
2020-04-12 18:23:29 +0700

    change the random w by divide sqrt(num_of_nodes_of_prev_layer) trick

--- Application_of_Deep_NN/N-layers.py
+++ Application_of_Deep_NN/N-layers.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from Application_of_Deep_NN.dnn_app_utils_v3 import load_data, print_mislabeled_images
+from Application_of_Deep_NN.dnn_app_utils_v3 import load_data
 from Deep_NN.deep_NN_model import L_model_backward, L_model_forward, compute_cost, update_parameters, \
     initialize_parameters_deep, predict
 
@@ -15,11 +15,11 @@ test_x_flatten = test_x_orig.reshape(test_x_orig.shape[0], -1).T
 train_x = train_x_flatten / 255.
 test_x = test_x_flatten / 255.
 
+### CONSTANTS ###
 layers_dims = [12288, 20, 7, 5, 1]  # 4-layer model
 
 
-# GRADED FUNCTION: L_layer_model
-def L_layer_model(X, Y, layers_dims, learning_rate=0.75, num_iterations=3000, print_cost=False):  # lr was 0.009
+def L_layer_model(X, Y, layers_dims, learning_rate=0.0075, num_iterations=3000, print_cost=False):  # lr was 0.009
     np.random.seed(1)
     costs = []  # keep track of cost
     parameters = initialize_parameters_deep(layers_dims)
@@ -35,16 +35,15 @@ def L_layer_model(X, Y, layers_dims, learning_rate=0.75, num_iterations=3000, pr
             print("Cost after iteration %i: %f" % (i, cost))
         if print_cost and i % 100 == 0:
             costs.append(cost)
-
     plt.plot(np.squeeze(costs))
     plt.ylabel('cost')
     plt.xlabel('iterations (per hundreds)')
     plt.title("Learning rate =" + str(learning_rate))
     plt.show()
+
     return parameters
 
 
 parameters = L_layer_model(train_x, train_y, layers_dims, num_iterations=2500, print_cost=True)
 pred_train = predict(train_x, train_y, parameters)
 pred_test = predict(test_x, test_y, parameters)
-print_mislabeled_images(classes, test_x, test_y, pred_test)
